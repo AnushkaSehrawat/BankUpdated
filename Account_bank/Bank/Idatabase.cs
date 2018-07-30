201f40291c1e04cf6f578129625f554aee945ddb
tavisca-anushka <asehrawat@tavisca.com>
2018-07-30 18:32:21 +0530

    final

---
+++ Account_bank/Bank/Idatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    interface Idatabase
+    {
+        void Add_account();
+        void deposit();
+        void withdraw();
+        void interest();
+        void Display();
+
+    }
+}
